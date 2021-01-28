d1b1a88380ad8694699a9fddd6bf07d5a75d66af
Lupoglazik2 <78148946+Lupoglazik2@users.noreply.github.com>
2021-01-28 18:36:38 +0300

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -74,7 +74,7 @@ const TemplateWrapper = ({ children }) => {
                   <a
                     key={profile.profileType}
                     href={profile.url}
-                    title="Кто тут"
+                    title={profile.moto}
                     target="blank"
                     className={`social social--${profile.profileType.toLowerCase()}`}
                   >
